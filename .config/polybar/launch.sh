7e7b7ea6d501bd3dd448ce20adb5352c7d4b0b02
Gabriel Musskopf <gabrielgmusskopf@gmail.com>
2024-06-13 18:38:06 -0300

    feat: adicionar fonte que suporta japonês

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,7 +10,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar --list-monitors | while IFS=$'\n' read line; do
   monitor=$(echo $line | cut -d':' -f1)
   primary=$(echo $line | cut -d' ' -f3)
-  MONITOR=$monitor polybar --reload "root" &
   MONITOR=$monitor polybar --reload "top${primary:+"-primary"}" &
-  MONITOR=$monitor polybar --reload "top2" &
 done
--- .config/polybar/scripts/scroll_spotify_status.sh
+++ .config/polybar/scripts/scroll_spotify_status.sh
@@ -3,6 +3,7 @@
 # see man zscroll for documentation of the following parameters
 zscroll -l 30 \
         --delay 0.1 \
+        --before-text "  " \
         --scroll-padding " " \
         --match-command "`dirname $0`/get_spotify_status.sh --status" \
         --match-text "Playing" "--scroll true" \
